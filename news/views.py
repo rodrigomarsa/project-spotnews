5d1536674af0aa0dcf2eec7d23bac9b5083581ef
Rodrigo <rodrigomartins.agro@gmail.com>
2023-08-22 17:15:04 -0300

    feat: add news_details template

--- news/views.py
+++ news/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from news.models.news_model import News
+from django.http import Http404
 
 
 def index(request):
     context = {"all_news": News.objects.all()}
     return render(request, "home.html", context)
+
+
+def news_details(request, news_id):
+    try:
+        news = get_object_or_404(News, id=news_id)
+        return render(request, "news_details.html", {"news": news})
+    except Http404:
+        return render(request, "404.html")

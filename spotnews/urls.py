9a3f4a2a9806b30e690f04b3f12d0a5a0bc283f3
Rodrigo <rodrigomartins.agro@gmail.com>
2023-08-23 21:02:23 -0300

    feat: add api/news

--- spotnews/urls.py
+++ spotnews/urls.py
@@ -20,12 +20,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework import routers
 from news_rest.views.categories_view import CategoryViewSet
+from news_rest.views.news_view import NewsViewSet
 from news_rest.views.users_view import UserViewSet
 
 
 router = routers.DefaultRouter()
 router.register(r"categories", CategoryViewSet)
 router.register(r"users", UserViewSet)
+router.register(r"news", NewsViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
